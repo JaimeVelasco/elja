804c87f86013e30719a88994d6f22b500982ffb8
JaimeVelasco <contact@jaimevelas.co>
2019-11-21 11:37:46 -0600

    Access GSheets

---
+++ src/components/GSheets.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <p>Google Sheets API Quickstart</p>
+    <button id="authorize_button" style="display: none;">Authorize</button>
+    <button id="signout_button" style="display: none;">Sign Out</button>
+    <pre id="content" style="white-space: pre-wrap;"></pre>
+    <div v-if="isSignedIn">
+      <li v-for="(cliente, index) in clientes" v-bind:key=index>
+        {{ cliente[0] }}
+        {{ cliente[1] }}
+        {{ cliente[2] }}
+        {{ cliente[3] }}
+        {{ cliente[4] }}
+      </li>
+    </div>
+
+    <h1 v-else>Oh no 😢, no estas autorizadx.</h1>
+  </div>
+</template>
+
+<script>
+// If the sheet is private, we'll have to authenticate.
+const SPREADSHEET_ID = '1lti3IWGxHMTJLB4hvezerNCeeE5LKfzduqq-X2P_2gg'
+
+export default {
+  name: 'GSheets',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      clientes: []
+    }
+  },
+  created () {
+    this.login()
+  },
+  computed: {
+    isSignedIn () {
+      return this.$isSignedIn()
+    }
+  },
+  methods: {
+    login () {
+      let vm = this
+      if (this.$isAuthenticated() !== true) {
+        this.$login()
+      }
+      this.$getGapiClient()
+        .then(gapi => {
+          gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: SPREADSHEET_ID,
+            range: 'JAIME!A3:Z640',
+            majorDimension: 'ROWS'
+          }).then((response) => {
+            const result = response.result
+            vm.clientes = result.values
+          })
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import VueGAPI from 'vue-gapi'
+
+console.log('..', process.env.VUE_APP_API_KEY)
+console.log('..', process.env.VUE_APP_CLIENT_ID)
+
+const apiConfig = {
+  apiKey: process.env.VUE_APP_API_KEY,
+  clientId: process.env.VUE_APP_CLIENT_ID,
+  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  scope: 'https://www.googleapis.com/auth/spreadsheets'
+  // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
+}
 
 Vue.config.productionTip = false
+
+Vue.use(VueGAPI, apiConfig)
 
 new Vue({
   router,
