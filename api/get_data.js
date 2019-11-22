52b53f907f83f6726794dbaa5438f5702604c5a1
JaimeVelasco <contact@jaimevelas.co>
2019-11-21 20:21:56 -0600

    Get data from serverless function

--- api/get_data.js
+++ api/get_data.js
@@ -1,4 +1,14 @@
-module.exports = (req, res) => {
-  const { name = 'World' } = req.query
-  res.status(200).send(`Hello ${name}!`)
+const asciify = require('asciify-image')
+
+module.exports = async (req, res) => {
+  if (!req.query.username) {
+    return res.status(400).send('username is missing from query parameters')
+  }
+
+  const avatarUrl = `https://github.com/${req.query.username}.png`
+
+  const opts = { fit: 'box', width: 40, height: 40 }
+  const asciified = await asciify(avatarUrl, opts)
+
+  return res.status(200).send(asciified)
 }
